28013d87510a399c11fa9955724dac9b2548b40c
Uladzimir_Zaitsau <uladzimir_zaitsau1@epam.com>
2019-11-04 00:21:23 +0300

    fix logger

--- src/service/proxy/logger.js
+++ src/service/proxy/logger.js
@@ -2,10 +2,10 @@ export default function LoggerDecorator(instance) {
     return new Proxy(instance, 
     { 
         get: function(target, prop, receiver) {
-            
+
         if(typeof target[prop] === 'function') {
             return function(...args) {
-                console.log(`called ${prop} with args: `, args);
+                console.log(`called ${prop} with args: `, ...args);
                 return target[prop](...args);
             }
         } 
